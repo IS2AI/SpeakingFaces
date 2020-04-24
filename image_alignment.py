5573009ce2364189e79d18ac9a99fde814f14d18
Madina Abdrakhmanova <madina.bulatovna@gmail.com>
2020-04-24 13:44:42 +0600

    added more comments to image_alignment.py

--- image_alignment.py
+++ image_alignment.py
@@ -9,22 +9,22 @@ import cv2
 import argparse
 import os
 
-# construct the argument parse and parse the arguments
+# parse the provided arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
-	help="path to dataset")
+	help="path to the dataset")
 ap.add_argument("-n", "--frame", type=int, default=1,
 	help="process every n'th frame")
 ap.add_argument("-i", "--sub_info",  nargs='+', type=int,
-	help="subject info: ID, trial #")
+	help="subID(1,...,142) trialID (1,2) posID(1,...,9)")
 ap.add_argument("-y", "--dy",  nargs='+', type=int,
-	help="dy shifts based on positions")
+	help="a list of shifts in y axis for each position")
 ap.add_argument("-x", "--dx",  nargs='+', type=int,
-	help="x shifts based on positions")
+	help="a list of shifts in x axis for each position")
 ap.add_argument("-s", "--show", type=int, default=0,
-	help="visualize ext racted faces")
+	help="visualize or not a preliminary result of alignment")
 ap.add_argument("-e", "--session", type =int, default = 1,
-        help="session_id, 1 for nonspeaking, 2 for speaking")
+        help="sessionID, 1 for nonspeaking, 2 for speaking")
 args = vars(ap.parse_args())
 
 # load matched features from xlsx file and convert it numpy array 
@@ -35,14 +35,14 @@ M = df.to_numpy()
 opt = "_cmd" if args["session"] == 2 else ""
 
 # grab the path to the visual images in our dataset
-dataset_path = "{}sub_{}".format(args["dataset"], args["sub_info"][0])+os.path.sep+"trial_{}".format(args["sub_info"][1])  
-rgb_image_filepaths = list(paths.list_images(dataset_path + os.path.sep+"rgb_image{}".format(opt)))
+sub_trial_dataset_path = "{}sub_{}".format(args["dataset"], args["sub_info"][0])+os.path.sep+"trial_{}".format(args["sub_info"][1])  
+rgb_image_filepaths = list(paths.list_images(sub_trial_dataset_path + os.path.sep+"rgb_image{}".format(opt)))
 
-# create a directory to save images
-rgb_image_aligned_path = dataset_path + os.path.sep+"rgb_image{}_aligned".format(opt)+os.path.sep
+# create a directory to save aligned images
+rgb_image_aligned_path = sub_trial_dataset_path + os.path.sep+"rgb_image{}_aligned".format(opt)+os.path.sep
 make_dir(rgb_image_aligned_path)
 
-# loop over images in the folders
+# loop over original visible images in the folders
 for rgb_image_filepath in rgb_image_filepaths:
 
     # extract the current image info
@@ -53,11 +53,8 @@ for rgb_image_filepath in rgb_image_filepaths:
     else:
         sub_id, trial_id, session_id, pos_id, command_id, frame_id = rgb_image_filepath.split(os.path.sep)[-1].split("_")[-7:-1]
         rgb_image_aligned_filepath = "{}{}_{}_{}_{}_{}_{}_3.png".format(rgb_image_aligned_path, sub_id, trial_id, session_id, pos_id, command_id, frame_id)
-    #print(rgb_image_aligned_filepath)
-    #print(rgb_image_filepath)
-    # given by the argument only if "show" mode
-    # is enabled
-
+    
+    # process only the files for the given position if "show" mode is enabled. 
     if args["sub_info"][2] != int(pos_id) and args["show"]:
         cv2.destroyAllWindows()
         continue
@@ -77,23 +74,22 @@ for rgb_image_filepath in rgb_image_filepaths:
         print("[INFO] processing image {}".format(rgb_image_filepath.split(os.path.sep)[-1]))
 
         # construct the thermal image path using the rgb image path
-        thr_image_path = path_to_thermal_image(rgb_image_filepath, dataset_path, os.path.sep+"thr_image{}".format(opt)+os.path.sep)
+        thr_image_filepath = path_to_thermal_image(rgb_image_filepath, sub_trial_dataset_path, os.path.sep+"thr_image{}".format(opt)+os.path.sep)
+        
         # load rgb and corresponding thermal image 
         rgb = cv2.imread(rgb_image_filepath)
-        thr = cv2.imread(thr_image_path)
+        thr = cv2.imread(thr_image_filepath)
 
         # grab a size of the thermal image 
         (H_thr, W_thr) = thr.shape[:2]
         
-        # warp the rgb image
-        # to align with the thermal image
+        # warp the rgb image to align with the thermal image
         rgb = cv2.warpPerspective(rgb, H, (W_thr, H_thr), 
                 flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
         
         if args["show"]:
             # make a copy of the rgb image
-            # then replace its RED channel with 
-            # the RED channel of the thermal image
+            # then replace its RED channel with the RED channel of the thermal image
             rgb_copy = rgb.copy()
             rgb_copy[:, :, 2] = thr[:, :, 2]
 
@@ -101,8 +97,9 @@ for rgb_image_filepath in rgb_image_filepaths:
             cv2.imshow("Sub:{} Trial:{} Session:{} Pos:{} Command:{} Frame:{}".format(sub_id, trial_id, session_id, pos_id, command_id, frame_id), np.hstack([rgb, thr, rgb_copy]))
             key = cv2.waitKey(0) & 0xFF
 
-            # if the 'q' key is pressed, stop the loop
+            # if the 'q' key is pressed, stop the loop, exit from the program
             if key == ord("q"):
                     break
-    
+        # note the images are written if only show is disabled; 
+        # all positions are processed at this point assuming that the optimal shifts have been found and provided
         cv2.imwrite(rgb_image_aligned_filepath, rgb)
