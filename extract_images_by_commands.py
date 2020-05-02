ebfce654efab334c888476e1af4cbc2f8358a0a0
Madina Abdrakhmanova <madina.bulatovna@gmail.com>
2020-05-02 16:28:12 +0600

    refactored the scripts

--- extract_images_by_commands.py
+++ extract_images_by_commands.py
@@ -3,97 +3,70 @@ import glob
 import argparse
 import cv2
 from speakingfacespy.imtools import make_dir
+import scipy.io.wavfile
 
-def extract_frame(audio_trim_filepath, stream_id):
-    print('[INFO] reading already trimmed audio file: '+audio_trim_filepath)
-    sample_rate_trim, audio_trim = scipy.io.wavfile.read(audio_trim_filepath)
-    duration_trim = audio_trim.shape[0] / sample_rate_trim
-    print('[INFO] duration of the already trimmed file = {}'.format(duration_trim))
-      
-    audio_trim_filepath_split = audio_trim_filepath.split('/')
-    audio_trim_filename_split = audio_trim_filepath_split[-1].split('_')
-    video_to_extract_filename  = '_'.join(audio_trim_filename_split[:-1])+'_{}.avi'.format(stream_id)
+def extract_frame(audio_trim_filename, duration, data_path, stream_id):
     
+    video_to_extract_filename  = '_'.join(audio_trim_filename.split('_')[:-1])+'_{}.avi'.format(stream_id)
     opt = 'thr' if stream_id == 1 else 'rgb'  
-    video_to_extract_filepath = '/'.join(audio_trim_filepath_split[:-2])+'/'+opt+'_video_cmd/'+video_to_extract_filename
+    video_to_extract_filepath = data_path + opt+ '_video_cmd' + os.path.sep + video_to_extract_filename
     print('[INFO] accessing a video file:'+ video_to_extract_filepath)
-    cap =  cv2.VideoCapture(video_to_extract_filepath)
+    extracted_images_dir = data_path + opt+ '_image_cmd'+ os.path.sep
     
-    extracted_images_dir = '/'.join(audio_trim_filepath_split[:-2])+'/'+opt+'_image_cmd/'
-    fps = 28
-    max_num_frames = int(duration_trim*28)
-    frameID=1
-    while(cap.isOpened() and frameID <= max_num_frames):
+    cap =  cv2.VideoCapture(video_to_extract_filepath)
+    max_num_frames = int(duration*28)
+    frame_id=1
+    while(cap.isOpened() and frame_id <= max_num_frames):
         ret, frame = cap.read()
         if ret == False:
             break
-        extracted_image_filename = '_'.join(audio_trim_filename_split[:-1])+'_{}_{}.png'.format(frameID, stream_id)
+        extracted_image_filename = '_'.join(audio_trim_filename.split('_')[:-1])+'_{}_{}.png'.format(frame_id, stream_id)
         extracted_image_filepath = extracted_images_dir+extracted_image_filename
         if not os.path.exists(extracted_image_filepath):
-            if frameID%10 == 0:
+            if frame_id%10 == 0:
                 print('[INFO] saving an extracted frame: '+ extracted_image_filepath)
             cv2.imwrite(extracted_image_filepath, frame)
-        frameID+=1
+        frame_id+=1
     
     cap.release()
     cv2.destroyAllWindows()
     print('[INFO] finished extracting frames')
-
-
-def extract_frame_new(audio_trim_filepath, dataset_path, set_name, sub_id, trial_id, stream_id):
-    print('[INFO] reading already trimmed audio file: '+audio_trim_filepath)
-    sample_rate_trim, audio_trim = scipy.io.wavfile.read(audio_trim_filepath)
-    duration_trim = audio_trim.shape[0] / sample_rate_trim
-    print('[INFO] duration of the already trimmed file = {}'.format(duration_trim))
-      
-    audio_trim_filepath_split = audio_trim_filepath.split('/')
-    audio_trim_filename_split = audio_trim_filepath_split[-1].split('_')
-    video_to_extract_filename  = '_'.join(audio_trim_filename_split[:-1])+'_{}.avi'.format(stream_id)
     
-    opt = 'thr' if stream_id == 1 else 'rgb'  
-    video_to_extract_filepath = '/'.join(audio_trim_filepath_split[:-2])+'/'+opt+'_video_cmd/'+video_to_extract_filename
-    print('[INFO] accessing a video file:'+ video_to_extract_filepath)
-    cap =  cv2.VideoCapture(video_to_extract_filepath)
-    
-    extracted_images_dir = '/'.join(audio_trim_filepath_split[:-2])+'/'+opt+'_image_cmd/'
-    fps = 28
-    max_num_frames = int(duration_trim*28)
-    frameID=1
-    while(cap.isOpened() and frameID <= max_num_frames):
-        ret, frame = cap.read()
-        if ret == False:
-            break
-        extracted_image_filename = '_'.join(audio_trim_filename_split[:-1])+'_{}_{}.png'.format(frameID, stream_id)
-        extracted_image_filepath = extracted_images_dir+extracted_image_filename
-        if not os.path.exists(extracted_image_filepath):
-            if frameID%10 == 0:
-                print('[INFO] saving an extracted frame: '+ extracted_image_filepath)
-            cv2.imwrite(extracted_image_filepath, frame)
-        frameID+=1
-    
-    cap.release()
-    cv2.destroyAllWindows()
-    print('[INFO] finished extracting frames')
 
 def extract_frames_by_sub_trial(dataset_path, set_name, sub_id, trial_id):
     #assuming every trial has mic1_audio_cmd_trim folder
     print("[INFO] extract frames from videos by commands for sub_id = {}, trial_id = {}".format(sub_id, trial_id))
-    audio_trim_filepaths = glob.glob('{}{}_data/sub_{}/trial_{}/mic1_audio_cmd_trim/*.wav'.format(dataset_path, set_name, sub_id, trial_id))
-    make_dir('{}{}_data/sub_{}/trial_{}/thr_image_cmd'.format(dataset_path, set_name, sub_id, trial_id))
-    make_dir('{}{}_data/sub_{}/trial_{}/rgb_image_cmd'.format(dataset_path, set_name, sub_id, trial_id))
-
+    data_path = '{}{}_data{}sub_{}{}trial_{}{}'.format(
+        dataset_path, set_name, os.path.sep, sub_id, 
+        os.path.sep, trial_id, os.path.sep)
+    
+    audio_trim_filepaths = glob.glob(data_path +'mic1_audio_cmd_trim'+os.path.sep+'*.wav')
+    make_dir(data_path+'thr_image_cmd/')
+    make_dir(data_path+'rgb_image_cmd/')
+    print(data_path +'mic1_audio_cmd_trim'+os.path.sep+'*.wav')
     for audio_trim_filepath in audio_trim_filepaths:
-        extract_frame(audio_trim_filepath, 1)
-        extract_frame(audio_trim_filepath, 2)    
+        audio_trim_filename = audio_trim_filepath.split(os.path.sep)[-1]
+        print('[INFO] reading already trimmed audio file: '+audio_trim_filename)
+        sample_rate_trim, audio_trim = scipy.io.wavfile.read(audio_trim_filepath)
+        duration_trim = audio_trim.shape[0] / sample_rate_trim
+        print('[INFO] duration of the already trimmed file = {}'.format(duration_trim))
+        
+        extract_frame(audio_trim_filename, duration_trim, data_path, 1)
+        extract_frame(audio_trim_filename, duration_trim, data_path, 2)   
+
 
 def extract_frames_by_set(dataset_path, set_name):
-    csv_paths = glob.glob('{}csvs/{}_set/fpc5/*1.csv'.format(dataset_path, set_name))
-    csv_paths = csv_paths+ glob.glob('{}csvs/{}_set/fpc5/*2.csv'.format(dataset_path, set_name))
-    for csv_path in csv_paths:
-        csv_filename_split = csv_path.split('/')[-1].split('_')
+    csv_filepaths = glob.glob('{}csvs{}{}_set{}fpc5{}*1.csv'.format(
+        dataset_path,os.path.sep,set_name,os.path.sep,os.path.sep))
+    csv_filepaths = csv_filepaths+ glob.glob('{}csvs{}{}_set{}fpc5{}*2.csv'.format(
+        dataset_path,os.path.sep,set_name,os.path.sep,os.path.sep))
+    
+    for csv_filepath in csv_filepaths:
+        csv_filename_split = csv_filepath.split(os.path.sep)[-1].split('_')
         trial_id = int(csv_filename_split[-1][-5])
         sub_id = int(csv_filename_split[1][3:])
         extract_frames_by_sub_trial(dataset_path, set_name, sub_id, trial_id)
+
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
--- trim_audio.py
+++ trim_audio.py
@@ -4,50 +4,54 @@ import scipy.io.wavfile
 import argparse
 from speakingfacespy.imtools import make_dir
 
-def trim_audio(audio_trim_path, length_audio_trim,to_trim_mic_id, to_trim_new_dir):
+def trim_audio(audio_trim_filepath, length_audio_trim,to_trim_mic_id, to_trim_new_dir):
     #trim the corresponding other mic audio
-    audio_trim_filename = audio_trim_path.split(os.path.sep)[-1] 
+    audio_trim_filename = audio_trim_filepath.split(os.path.sep)[-1] 
     audio_trim_filename_split = audio_trim_filename.split('_') 
     audio_to_trim_filename  = '_'.join(audio_trim_filename_split[:-1])+'_{}.wav'.format(to_trim_mic_id)
-    audio_to_trim_new_path = to_trim_new_dir+os.path.sep+audio_to_trim_filename
+    audio_to_trim_new_filepath = to_trim_new_dir+os.path.sep+audio_to_trim_filename
     
-    if not os.path.exists(audio_to_trim_new_path):
+    if not os.path.exists(audio_to_trim_new_filepath):
         print('[INFO] reading an audio file to trim: '+audio_to_trim_filename)
         sample_rate_to_trim, audio_to_trim = scipy.io.wavfile.read(to_trim_new_dir[:-5]+os.path.sep+audio_to_trim_filename)
         duration_to_trim = audio_to_trim.shape[0] / sample_rate_to_trim
         print('[INFO] duration of the original file = {}'.format(duration_to_trim))
-        print('[INFO] writing trimmed audio to {}'.format(audio_to_trim_new_path))
-        scipy.io.wavfile.write(audio_to_trim_new_path, sample_rate_to_trim, audio_to_trim[:length_audio_trim])
+        print('[INFO] writing trimmed audio to {}'.format(audio_to_trim_new_filepath))
+        scipy.io.wavfile.write(audio_to_trim_new_filepath, sample_rate_to_trim, audio_to_trim[:length_audio_trim])
         print('[INFO] done trimming')
     else:
-        print('[INFO] the file {} already exists'.format(audio_to_trim_new_path))
+        print('[INFO] the file {} already exists'.format(audio_to_trim_new_filepath))
         
 
 def trim_audio_by_sub_trial(dataset_path, set_name, sub_id, trial_id):
     print("[INFO] trim all audio files given sub_id = {}, trial_id = {}".format(sub_id, trial_id))
-    audio_trim_paths = glob.glob('{}{}_data{}sub_{}{}trial_{}{}*_trim{}*.wav'.format(
+    audio_trim_filepaths = glob.glob('{}{}_data{}sub_{}{}trial_{}{}*_trim{}*.wav'.format(
         dataset_path, set_name, os.path.sep, sub_id, os.path.sep, trial_id, os.path.sep, os.path.sep))        
-    is_mic1_audio_trim = (audio_trim_paths[0].split(os.path.sep)[-2].find('1')!=-1)
+    is_mic1_audio_trim = (audio_trim_filepaths[0].split(os.path.sep)[-2].find('1')!=-1)
     to_trim_mic_id = 2 if is_mic1_audio_trim else 1
     to_trim_dir = '{}{}_data{}sub_{}{}trial_{}{}mic{}_audio_cmd_trim'.format(
         dataset_path, set_name, os.path.sep, sub_id, os.path.sep, trial_id, os.path.sep, to_trim_mic_id)
+    
     if not os.path.exists(to_trim_dir):
         make_dir(to_trim_dir)
-        for audio_trim_path in audio_trim_paths:
-            print('[INFO] reading already trimmed audio file: '+audio_trim_path)
-            sample_rate_trim, audio_trim = scipy.io.wavfile.read(audio_trim_path)
+        for audio_trim_filepath in audio_trim_filepaths:
+            print('[INFO] reading already trimmed audio file: '+audio_trim_filepath)
+            sample_rate_trim, audio_trim = scipy.io.wavfile.read(audio_trim_filepath)
             length_audio_trim = audio_trim.shape[0]
             duration_trim = audio_trim.shape[0] / sample_rate_trim
             print('[INFO] duration of the already trimmed file = {}'.format(duration_trim))
             #trim the corresponding other mic audio
-            trim_audio(audio_trim_path, length_audio_trim, to_trim_mic_id, to_trim_dir)
+            trim_audio(audio_trim_filepath, length_audio_trim, to_trim_mic_id, to_trim_dir)
 
 
 def trim_audio_by_set(dataset_path, set_name):
-    csv_paths = glob.glob('{}csvs{}{}_set{}fpc5{}*1.csv'.format(dataset_path,os.path.sep,set_name,os.path.sep,os.path.sep))
-    csv_paths = csv_paths+ glob.glob('{}csvs{}{}_set{}fpc5{}*2.csv'.format(dataset_path,os.path.sep,set_name,os.path.sep,os.path.sep))
-    for csv_path in csv_paths:
-        csv_filename_split = csv_path.split(os.path.sep)[-1].split('_')
+    csv_filepaths = glob.glob('{}csvs{}{}_set{}fpc5{}*1.csv'.format(
+        dataset_path,os.path.sep,set_name,os.path.sep,os.path.sep))
+    csv_filepaths = csv_filepaths+ glob.glob('{}csvs{}{}_set{}fpc5{}*2.csv'.format(
+        dataset_path,os.path.sep,set_name,os.path.sep,os.path.sep))
+    
+    for csv_filepath in csv_filepaths:
+        csv_filename_split = csv_filepath.split(os.path.sep)[-1].split('_')
         trial_id = int(csv_filename_split[-1][-5])
         sub_id = int(csv_filename_split[1][3:])
         trim_audio_by_sub_trial(dataset_path, set_name, sub_id, trial_id)    
