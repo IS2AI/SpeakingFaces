85c66b5a64c46b84e42f7dfb9c4c1ac5e21b1b4b
Askat Kuzdeuov <askhatkuzdeuov@gmail.com>
2020-08-06 18:08:58 +0600

    Update build_pix2pix_data.py

--- build_pix2pix_data.py
+++ build_pix2pix_data.py
@@ -8,7 +8,7 @@ import cv2
 import os
 import argparse
 
-# construct the argument parse and parse the arguments
+# construct argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
 	help="path to the SpeakingFaces dataset")
@@ -17,39 +17,42 @@ ap.add_argument("-m", "--model", type=str, default="dnn",
 ap.add_argument("-c", "--confidence", type=float, default=0.9,
 	help="a minimum probability for dnn to filter weak detections")
 ap.add_argument("-n", "--frame", type=int, default=100,
-	help="process only every n'th frame")
+	help="process only n'th frame")
 ap.add_argument("-s", "--show", type=int, default=0,
 	help="visualize extracted faces")
 args = vars(ap.parse_args())
 
-# load dnn face detector from disk
-# if it was selected
+# load the serialiazed dnn face detector from disk
+# in case if it was selected
 if args["model"] == "dnn": 
 	print("[INFO] loading dnn face detector...")
 	face_net = cv2.dnn.readNetFromCaffe("models/deploy.prototxt.txt", 
 		"models/res10_300x300_ssd_iter_140000.caffemodel")
 
+# initialize a path to our dataset
+path_to_dataset = args["dataset"]
+
 # create directories to save train data
-train_path = os.path.join(args["dataset"], "pix2pix/train")
+train_path = os.path.join(path_to_dataset, "pix2pix/train")
 make_dir(train_path)
 
 # validation data
-val_path = os.path.join(args["dataset"], "pix2pix/val")
+val_path = os.path.join(path_to_dataset, "pix2pix/val")
 make_dir(val_path)
 
 # and testing data
-test_path = os.path.join(args["dataset"], "pix2pix/test")
+test_path = os.path.join(path_to_dataset, "pix2pix/test")
 make_dir(test_path)
 
-# initialize the total number of subjects,
-# trials, and positions
+# initialize a total number of subjects, trials per subject, 
+# positions per trial, and frames per position
 sub_ids = 142
 trial_ids = 2
 pos_ids = 9
 frame_ids = 900
 
 # initialize a total number of subjects 
-# for training and testing
+# for training, validation, and testing
 num_train_subjects = 100
 num_val_subjects = 20
 num_test_subjects = 22
@@ -59,30 +62,29 @@ train_images = 0
 val_images = 0
 test_images = 0
 
-# read information about subjects
-# from CSV file to NumPy array
-# sub_id - 'train/val/test' - age - gender - ...
+# read information about subjects from a .csv file 
+# to Pandas table and convert it to NumPy array
 sub_info = pd.read_csv('metadata/subjects.csv').to_numpy()
 
 # loop over the subjects 1...142
 for sub_id in range(1, sub_ids + 1):
-	# skip african subjects 
+	# skip Black ethnicities  
 	ethnicity = sub_info[sub_id - 1, 4]
 	if ethnicity == "Black":
 		continue
 
 	# loop over the trials 1..2
 	for trial_id in range(1, trial_ids + 1):
-		# skip subjects with accecories
+		# skip subjects with accessories
 		acc = sub_info[sub_id - 1, trial_id + 4]
 		if acc != "None":
 			continue
 
-		# construct path to the folders with rgb and thermal images
-		if sub_id <= num_train_subjects:
+		# construct paths to the folders with rgb and thermal images
+		if sub_id <= 100:
 			rgb_path = os.path.join(args["dataset"], "train_data/sub_{}/trial_{}/rgb_image_aligned".format(sub_id, trial_id))
 			thr_path = os.path.join(args["dataset"], "train_data/sub_{}/trial_{}/thr_image".format(sub_id, trial_id))
-		elif sub_id > num_train_subjects and sub_id <= num_train_subjects + num_val_subjects:
+		elif sub_id > 100 and sub_id <= 120:
 			rgb_path = os.path.join(args["dataset"], "valid_data/sub_{}/trial_{}/rgb_image_aligned".format(sub_id, trial_id))
 			thr_path = os.path.join(args["dataset"], "valid_data/sub_{}/trial_{}/thr_image".format(sub_id, trial_id))
 		else:
@@ -99,11 +101,11 @@ for sub_id in range(1, sub_ids + 1):
 					continue
 
 				print("[INFO] processing sub:{}, trial:{}, pos:{}, frame:{}".format(sub_id, trial_id, pos_id, frame_id))
-				# construct rgb and thermal  
+				# construct rgb and thermal filenames   
 				rgb_file = "{}_{}_1_{}_{}_{}.png".format(sub_id, trial_id, pos_id, frame_id, 3)
 				thr_file = "{}_{}_1_{}_{}_{}.png".format(sub_id, trial_id, pos_id, frame_id, 1)
 
-				# construct the source path
+				# construct the final paths to images
 				rgb_file = os.path.join(rgb_path, rgb_file)
 				thr_file = os.path.join(thr_path, thr_file)
 				#print(rgb_file)
@@ -113,8 +115,8 @@ for sub_id in range(1, sub_ids + 1):
 				rgb_image = cv2.imread(rgb_file)		
 				thr_image = cv2.imread(thr_file)
 
-				# detect the (x ,y)-coordinates of the bounding boxes
-				# corresponding to each face in the input image 
+				# detect faces in the rgb image and return 
+				# corresponding bounding boxes 
 				if args["model"] != "dnn":
 					rgb_boxes = face_recognition.face_locations(rgb_image, model=args["model"])
 				else:
@@ -122,8 +124,8 @@ for sub_id in range(1, sub_ids + 1):
 
 				# if at least one face is detected
 				if len(rgb_boxes):
-					# we assume that only one person was detected 
-					# extract corners of the bounding box
+					# assume that only one person was detected and extract (x,y) 
+					# coordinates of the corners of the bounding box
 					(startY, endX, endY, startX) = rgb_boxes[0]
 
 					# crop the detected faces 
@@ -149,6 +151,7 @@ for sub_id in range(1, sub_ids + 1):
 						if key == ord("q"):
 							break
 
+					# save the images
 					if sub_id <= num_train_subjects:
 						train_images += 1
 						cv2.imwrite(os.path.join(train_path, "{}_{}_{}_{}.png".format(sub_id, trial_id, pos_id, frame_id)), np.hstack([thr_face, rgb_face]))
